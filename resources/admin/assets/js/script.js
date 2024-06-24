bc42f52385113e0a8ac2c70c4a55c69ecb35b0c2
Diaz Mahendra <dmforaname#gmail.com>
2024-06-24 20:30:37 +0700

    setup adminLTE

---
+++ resources/admin/assets/js/script.js
@@ -0,0 +1,27 @@
+
+$(function () {
+
+    //$('.main-footer').addClass('text-sm');
+
+    //document.body.classList.add('sidebar-mini');
+    //document.body.classList.add('hold-transition');
+
+    console.log('script.js')
+
+    // $('body').addClass('sidebar-mini');
+    // $('body').addClass('hold-transition');
+
+    // $('.main-sidebar').removeClass('sidebar-dark-primary');
+    $('.main-header').removeClass('navbar-light');
+    $('.main-header').removeClass('navbar-white');
+
+    $('.nav-sidebar').addClass('nav-flat');
+    $('.main-header').addClass('navbar-gray');
+    $('.main-header').addClass('navbar-dark');
+    // $('.main-header').addClass('border-bottom-0');
+    // $('.main-sidebar').addClass('sidebar-light-lightblue');
+    // $('.brand-link').addClass('navbar-gray');
+    // $('.brand-link').addClass('text-light');
+    $('.nav-sidebar').addClass('nav-child-indent');
+    // $('.nav-sidebar').addClass('nav-compact')
+});
--- resources/admin/js/bootstrap.js
+++ resources/admin/js/bootstrap.js
@@ -1,4 +1,20 @@
 import axios from 'axios';
+import  'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// window._ = require('lodash');
+
+try {
+   
+    window.$ = window.jQuery = require('jquery');
+   
+
+} catch (e) {}
+
+
+import 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/js/bootstrap.bundle.min.js'
+import '../assets/js/adminlte.min.js'
+// import '../assets/js/demo.js'
+import '../assets/js/script.js'
