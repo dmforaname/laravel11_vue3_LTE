823db778bd16ba29382e235132e4633514af0de3
Widiasa Mahendra <dmforaname@gmail.com>
2024-08-12 14:23:35 +0700

    baseInput

---
+++ resources/admin/components/Forms/baseInput.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="form-group" :class="setCols(this.cols)">
+        <label>{{ label }} <span class="formRequired">{{ formRequired }}</span></label>
+        <input 
+        type="text" 
+        class="form-control" 
+        :placeholder="placeholder" 
+        @input="$emit('update:modelValue', $event.target.value)"
+        >
+        <small class="text-danger">{{ formAlert }}</small>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    props: {
+        cols : {
+            type : String,
+            default: '6'
+        },
+        placeholder : {
+            type : String,
+            default: ''
+        },
+        label : {
+            type : String,
+            default: ''
+        },
+        isRequired : {
+            type : Boolean,
+            default : true
+        },
+        modelValue : {
+            type : String,
+            default : ''
+        },
+        formAlert : {
+            type : String,
+            default : ''
+        }
+    },
+    data() {
+        return {
+            formRequired : "*",
+        }
+    },
+
+    setup() {
+        
+    },
+    methods: {
+        setCols(i) {
+            if (i) return `col-${i}`
+            return "col-6"
+        }
+    },
+    mounted() {
+        
+        if (this.isRequired === false) this.formRequired = ""
+    }
+}
+
+</script>
